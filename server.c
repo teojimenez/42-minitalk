c12002566964bbe56606cde74b34dc14abe25958
Teo Jimenez <teojimen@cbr3s1.42barcelona.com>
2024-01-26 13:57:23 +0100

    update 26/01 ~ fixing norm

--- server.c
+++ server.c
@@ -13,29 +13,72 @@
 #include "minitalk.h"
 
 
-void	receive_len(int int_nb, int i, int flag, char *result, int bit)
+void	receive_len(int *int_nb, int *i,int *flag , char **result, int bit)
 {
 	if(bit == SIGUSR1) {  // Cambia 1 por el valor de tu bit
-		int_nb = (int_nb << 1) | 1;
+		*int_nb = (*int_nb << 1) | 1;
 	}else if (bit == SIGUSR2)
 	{  // Cambia 0 por el valor de tu otro bit
-		int_nb = int_nb << 1;
+		*int_nb = *int_nb << 1;
 	}
 	// printf("%d", i);
 	i++;
-	if (i == 32) {  // Cambia 32 por el número de bits que deseas en tu entero
-		i = 0;
-		flag = 0;
+	if (*i == 32) {  // Cambia 32 por el número de bits que deseas en tu entero
+		*i = 0;
+		*flag = 0;
 		// printf("%d", int_nb);
-		result = (char *)malloc((int_nb + 1) * sizeof(char));
-		if(!result)
+		*result = (char *)malloc((*int_nb + 1) * sizeof(char));
+		if(!*result)
 			return ;
 	}
 }
 
-int	receive_bits(int int_nb, int i, char whole_c, char *result, int bit)
-{
-	if (bit == SIGUSR1) 
+// void	receive_bits(int int_nb, int i, int j, char whole_c, char *result, int bit, int flag)
+// {
+// 	// static int j;
+	
+// 	// j = 0;
+
+// 	if (bit == SIGUSR1) 
+// 		whole_c = (whole_c << 1) | 1;
+// 	else if (bit == SIGUSR2)
+// 		whole_c = whole_c << 1;
+// 	i++;
+// 	if (i == 8)
+// 	{
+// 		i = 0;
+// 		result[j] = whole_c;
+// 		j++;
+// 		whole_c = 0;
+// 	}
+// 	if(j == int_nb)
+// 	{
+// 		result[j] = '\0';
+// 		ft_putstr_fd(result, 1);
+// 		result[0] = '\0';
+// 		free(result);
+// 		int_nb = 0;
+// 		i = 0;
+// 		whole_c = 0;
+// 		j = 0;
+// 		flag = 1;
+// 	}
+// }
+
+void    menu_receiver(int bit)
+{	
+	static int i = 0;
+    static int j = 0;
+    static int flag = 1;
+    static char whole_c = 0;
+    static int int_nb;
+    static char *result = NULL;
+
+    if (flag == 1)
+        receive_len(&int_nb, &i, &flag, &result, bit);
+    else
+	{
+			if (bit == SIGUSR1) 
 		whole_c = (whole_c << 1) | 1;
 	else if (bit == SIGUSR2)
 		whole_c = whole_c << 1;
@@ -55,29 +98,16 @@ int	receive_bits(int int_nb, int i, char whole_c, char *result, int bit)
 		free(result);
 		int_nb = 0;
 		i = 0;
-		flag = 1;
 		whole_c = 0;
 		j = 0;
+		flag = 1;
 	}
-}
-
-void    menu_receiver(int bit)
-{	
-	static int i = 0;
-    static int j = 0;
-    static int flag = 1;
-    static char whole_c = 0;
-    static int int_nb;
-    static char *result = NULL;
-
-    if (flag == 1) 
-        receive_len(int_nb, i, whole_c, result, bit);
-    else
-		flag = 
+	}
+		// receive_bits(int_nb, i, j, whole_c, result, bit, flag);
 
 }
 
-int main(void)
+int main(void)//mirar que el pid sea correcto
 {
 	ft_putstr_fd("PID: "C_GREEN, 1);
 	ft_putnbr_fd(getpid(), 1);
